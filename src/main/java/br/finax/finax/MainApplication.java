4fc1f2f09868b784fd294b14ec48f2475be91cea
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2023-10-20 23:55:01 -0300

    update package

--- src/main/java/br/finax/finax/MainApplication.java
+++ src/main/java/br/finax/finax/MainApplication.java
@@ -1,3 +1,5 @@
+package br.finax.finax;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
