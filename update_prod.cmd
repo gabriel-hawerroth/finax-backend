0108fa7a54320496ef15bdfdf294c4986117654b
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-04-18 13:46:13 -0300

    #: removed update script

--- update_prod.cmd
+++
@@ -1,9 +0,0 @@
-call mvn clean package
-
-ssh root@15.229.18.114 "pm2 delete api.finax"
-
-ssh root@15.229.18.114 "rm -rf /home/ubuntu/workspace/finax_api/*"
-
-scp -r D:\finax\Finax-backend\target\finax.jar root@15.229.18.114:/home/ubuntu/workspace/finax_api/
-
-ssh root@15.229.18.114 "/root/start_finax_api.sh"
