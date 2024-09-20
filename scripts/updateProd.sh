e93de5acd10845110a02c6cf2940b4cb7de334ab
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-09-20 13:22:26 -0300

    chore: added linux updateProd script

---
+++ scripts/updateProd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ..
+
+mvn -v
+
+# Executa o build usando Maven
+mvn clean package
+
+# Verifica se o build foi bem-sucedido
+if [ $? -ne 0 ]; then
+    echo "O build falhou. Parando a execução do script."
+    exit 1
+fi
+
+# Deleta o processo existente do PM2 no servidor remoto
+ssh root@15.229.18.114 "pm2 delete api.finax"
+
+# Remove os arquivos antigos no servidor remoto
+ssh root@15.229.18.114 "rm -rf /home/ubuntu/workspace/finax_api/*"
+
+# Copia o novo arquivo JAR para o servidor remoto
+scp -r target/finax.jar root@15.229.18.114:/home/ubuntu/workspace/finax_api/
+
+# Inicia o processo no servidor remoto
+ssh root@15.229.18.114 "/root/start_finax_api.sh"
