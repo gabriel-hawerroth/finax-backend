e00185bb630920843f290a332f8275c41165145a
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-11-08 14:14:41 -0300

    test: fix tests compilation error

--- src/test/java/br/finax/utils/InvoiceUtilsTest.java
+++ src/test/java/br/finax/utils/InvoiceUtilsTest.java
@@ -1,55 +1,57 @@
 package br.finax.utils;
 
+import br.finax.dto.FirstAndLastDate;
 import br.finax.exceptions.ServiceException;
-import br.finax.utils.InvoiceUtils.InvoiceCloseAndFirstDay;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static br.finax.utils.InvoiceUtils.getInvoiceCloseAndFirstDay;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 class InvoiceUtilsTest {
 
     @Test
     void testGetInvoiceCloseAndFirstDayWithDay31() {
-        InvoiceCloseAndFirstDay result = getInvoiceCloseAndFirstDay("03/2024", 31);
+        FirstAndLastDate result = getInvoiceCloseAndFirstDay("03/2024", 31);
 
-        assertEquals("2024-03-31", result.closeDay().toString());
+        assertEquals("2024-03-31", result.lastDay().toString());
         assertEquals("2024-03-02", result.firstDay().toString());
 
         result = getInvoiceCloseAndFirstDay("02/2024", 31);
 
-        assertEquals("2024-03-01", result.closeDay().toString());
+        assertEquals("2024-03-01", result.lastDay().toString());
         assertEquals("2024-02-01", result.firstDay().toString());
 
         result = getInvoiceCloseAndFirstDay("01/2024", 31);
 
-        assertEquals("2024-01-31", result.closeDay().toString());
+        assertEquals("2024-01-31", result.lastDay().toString());
         assertEquals("2024-01-01", result.firstDay().toString());
 
         result = getInvoiceCloseAndFirstDay("08/2024", 31);
 
-        assertEquals("2024-08-31", result.closeDay().toString());
+        assertEquals("2024-08-31", result.lastDay().toString());
         assertEquals("2024-08-01", result.firstDay().toString());
 
         result = getInvoiceCloseAndFirstDay("05/2024", 31);
 
-        assertEquals("2024-05-31", result.closeDay().toString());
+        assertEquals("2024-05-31", result.lastDay().toString());
         assertEquals("2024-05-02", result.firstDay().toString());
     }
 
     @Test
     void testGetInvoiceCloseAndFirstDaySuccess() {
-        InvoiceCloseAndFirstDay result = getInvoiceCloseAndFirstDay("08/2024", 2);
+        FirstAndLastDate result = getInvoiceCloseAndFirstDay("08/2024", 2);
 
-        assertEquals("2024-08-02", result.closeDay().toString());
+        assertEquals("2024-08-02", result.lastDay().toString());
         assertEquals("2024-07-03", result.firstDay().toString());
 
         result = getInvoiceCloseAndFirstDay("02/2024", 25);
 
-        assertEquals("2024-02-25", result.closeDay().toString());
+        assertEquals("2024-02-25", result.lastDay().toString());
         assertEquals("2024-01-26", result.firstDay().toString());
     }
 
