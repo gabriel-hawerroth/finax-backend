63fb2a07a1515e0662d1c1283270af8dae940007
gabriel-hawerroth <gabrielhawerroth04@gmail.com>
2025-07-22 23:12:17 -0300

    feat: implement database backup service and scheduled backup task

--- src/main/java/br/finax/enums/S3FolderPath.java
+++ src/main/java/br/finax/enums/S3FolderPath.java
@@ -5,11 +5,10 @@ import lombok.Getter;
 @Getter
 public enum S3FolderPath {
 
-    IMGS("imgs/"),
-    BANK_IMGS("imgs/banks/"),
     USER_PROFILE_IMG("user/profile-image/"),
     RELEASE_ATTACHMENTS("user/attachments/release/"),
-    INVOICE_PAYMENT_ATTACHMENTS("user/attachments/invoice_payment/");
+    INVOICE_PAYMENT_ATTACHMENTS("user/attachments/invoice_payment/"),
+    DATABASE_BACKUPS("backups/database/");
 
     private final String path;
 
