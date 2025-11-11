c9fbd8c2542a0e6b5adfb76c38e7c1b0dfa9f749
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2025-11-11 20:38:45 -0300

    feat: add updateDone method to manage release completion status

--- src/main/java/br/finax/controllers/ReleaseController.java
+++ src/main/java/br/finax/controllers/ReleaseController.java
@@ -1,5 +1,21 @@
 package br.finax.controllers;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import br.finax.dto.cash_flow.CashFlowValues;
 import br.finax.dto.cash_flow.MonthlyRelease;
 import br.finax.enums.release.DuplicatedReleaseAction;
@@ -8,12 +24,6 @@ import br.finax.services.ReleaseService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.net.URI;
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -89,6 +99,16 @@ public class ReleaseController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable long id, @RequestParam String duplicatedReleasesAction) {
         releaseService.delete(id, DuplicatedReleaseAction.valueOf(duplicatedReleasesAction));
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/update-done/{id}")
+    public ResponseEntity<Void> updateDone(
+            @PathVariable long id,
+            @RequestParam boolean done
+    ) {
+        releaseService.updateDone(id, done);
 
         return ResponseEntity.noContent().build();
     }
